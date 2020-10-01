c2fac60141d5fe06f66eeda66f9c4eee2032492c
younj1601 <younj1601@wrdsb.ca>
2020-10-01 15:48:29 +0000

    finished the forth question for 2.01

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *This program will ask for a positive interger then say that number in word form
+ * @author Jackson 
  */
 public class Main {
 
@@ -9,7 +10,51 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+  //create a scanner for user input
+  Scanner input = new Scanner(System.in);
+
+  //get the interger from the user
+  System.out.println("Please enter a positive integer:");
+  int num = input.nextInt();
+
+  //determine what the written form is
+  switch(num){
+    case 1:
+    System.out.println("one");
+    break;
+    case 2:
+    System.out.println("two");
+    break;
+    case 3:
+    System.out.println("three");
+    break;
+    case 4:
+    System.out.println("four");
+    break;
+    case 5:
+    System.out.println("five");
+    break;
+    case 6:
+    System.out.println("six");
+    break;
+    case 7:
+    System.out.println("seven");
+    break;
+    case 8:
+    System.out.println("eight");
+    break;
+    case 9:
+    System.out.println("nine");
+    break;
+    case 10:
+    System.out.println("ten");
+    break;
+    default:
+    System.out.println("not in the range");
+
+
+
+  }
     
   }
 }
